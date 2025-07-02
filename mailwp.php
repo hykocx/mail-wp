14fc10074dc902238659ddeeb69ca36fbb621dcb
Hyko <punk.flag8609@fastmail.com>
2025-07-02 11:45:56 -0400

    Update version number to 1.0.1

--- mailwp.php
+++ mailwp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MailWP
  * Description: Replace WordPress email function with SMTP or other email providers.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Hyko
  * Author URI: https://hyko.cx
  * Text Domain: mailwp
